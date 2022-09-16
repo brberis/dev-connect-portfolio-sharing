64b715cd66c03087aa5e7a45280cd5584fa6cc15
laurenp305 <lamor1800@gmail.com>
2022-09-15 20:48:30 -0400

    added html

---
+++ controllers/home-routes.js
@@ -0,0 +1,19 @@
+
+
+const router = require('express').Router();
+
+
+// get all posts for homepage
+router.get('/', (req, res) => {
+
+  })
+    .then(() => {
+    
+      res.render('homepage', {
+     
+      });
+    })
+
+module.export=router;
+
+ 
