2bbaa5ff9a9daf082364e6fc7012a8ebc7f53412
Cristobal Barberis <cristobal@barberis.com>
2022-09-17 00:34:52 -0400

    upvote model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const Project = require('./Project');
 const User = require('./User');
 const Comment = require('./Comment');
+const Vote = require('./Vote');
 
 // db associations
 User.hasMany(Project, {
@@ -17,8 +18,8 @@ Comment.belongsTo(User, {
   onDelete: 'SET NULL'
 });
 
-Comment.belongsTo(Post, {
-  foreignKey: 'post_id',
+Comment.belongsTo(Project, {
+  foreignKey: 'project_id',
   onDelete: 'SET NULL'
 });
 
@@ -28,7 +29,40 @@ User.hasMany(Comment, {
 });
 
 Project.hasMany(Comment, {
-  foreignKey: 'post_id'
+  foreignKey: 'project_id'
+});
+
+User.belongsToMany(Project, {
+  through: Vote,
+  as: 'voted_project',
+
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Project.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_project',
+  foreignKey: 'project_id',
+  onDelete: 'SET NULL'
+});
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Vote.belongsTo(Project, {
+  foreignKey: 'project_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Project.hasMany(Vote, {
+  foreignKey: 'project_id'
 });
 
 module.exports = { User, Project, Comment };
