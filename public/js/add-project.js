b881e108561c15f884889a66b52f5a0dff7b925d
mikejmarin25 <105763252+mikejmarin25@users.noreply.github.com>
2022-09-20 22:17:29 -0400

    create-project

--- public/js/add-project.js
+++ public/js/add-project.js
@@ -1,15 +1,25 @@
+const { Project } = require("../../models");
+
 //Add Project
 async function newFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="project-title"]').value;
     const description = document.querySelector('#description').value;
+    const image_url = document.querySelector('#image-url').value;
+    const github_url = document.querySelector('#github-url').value;
+    const date = document.querySelector('#date').value;
+    const public = document.querySelector('#public').value;
 
     const response = await fetch(`/api/projects`, {
         method: 'POST',
         body: JSON.stringify({
             title,
-            description
+            description,
+            image_url,
+            github_url,
+            date,
+            public,
         }),
         headers: {
             'Content-Type': 'application/json'
