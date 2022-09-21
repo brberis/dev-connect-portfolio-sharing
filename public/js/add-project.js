28a041d20b624b415f6313c38a9a131b5c9f9818
Cristobal Barberis <cristobal@barberis.com>
2022-09-20 21:29:18 -0400

    trix

--- public/js/add-project.js
+++ public/js/add-project.js
@@ -3,13 +3,13 @@ async function newFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="project-title"]').value;
-    const post_content = document.querySelector('#project_content').value;
+    const description = document.querySelector('#description').value;
 
     const response = await fetch(`/api/projects`, {
         method: 'POST',
         body: JSON.stringify({
             title,
-            post_content
+            description
         }),
         headers: {
             'Content-Type': 'application/json'
